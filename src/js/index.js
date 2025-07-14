134b828f583f4f72a479fe10e7c1105545a780de
kikeestrada <kikeestradadev@gmail.com>
2025-07-13 19:23:24 -0600

    adjustments

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@
 import coreModule from './core-modules/coreModule';
 /*here finish core layout ui scripts imports*/
 
-import Prism from 'prismjs';
 /*here start internal layout ui components scripts imports*/
 import internalModule from './internal-modules/internalModule';
 /*here finish internal layout ui components scripts imports*/
@@ -10,7 +9,6 @@ import internalModule from './internal-modules/internalModule';
 (() => {
 	/*here start core layout ui scripts functions*/
     coreModule();
-	Prism.highlightAll();
 	/*here finish core layout ui scripts functions*/
 })();
 
